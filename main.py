ee9a831c791c50111c527379807039c7dd329cd6
drafffffff <i0.0ia@qq.com>
2017-04-14 17:08:32 +0800

    e

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from F_zjzs import F_zjzs
-id = '33088120001013004X'
-pw = '20001013wx'
-id1 = '33088120000816892X'
-pw1 = '20000816xyx'
-zhoulongfei = F_zjzs()
-zhoulongfei.IdentityID = id
-zhoulongfei.PassWorld = pw
-zhoulongfei.get_Verifycode()
-# print zhoulongfei.cookie
-# print zhoulongfei.VerifyCode
-zhoulongfei.Tidy_Data()
-zhoulongfei.login()
-print zhoulongfei.result.cookies["appid"]
-zhoulongfei.default()
-print zhoulongfei.resultpage.text
-zhoulongfei.logout()
+
+
+if __name__ == "__main__":
+    id = '33088120001013004X'
+    pw = '20001013w'
+    id1 = '33088120000816892X'
+    pw1 = '20000816x'
+    zhoulongfei = F_zjzs()
+    zhoulongfei.IdentityID = id
+    zhoulongfei.PassWorld = pw
+    zhoulongfei.get_Verifycode()
+    # print zhoulongfei.cookie
+    print zhoulongfei.VerifyCode
+    zhoulongfei.Tidy_Data()
+    zhoulongfei.login()
+    print zhoulongfei.result.cookies["appid"]
+    zhoulongfei.default()
+    print zhoulongfei.resultpage.text
+    zhoulongfei.logout()
